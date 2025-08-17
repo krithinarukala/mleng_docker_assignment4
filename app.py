096d2f8e89d194ba492b5b987bb64f3540c6cdd1
Krithin Arukala <krithinarukala@gmail.com>
2025-08-17 15:38:16 -0500

    Committing several edits for the base files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 # Please customize the following variables 
-USER_NAME = "shahbazc"
+USER_NAME = "krithinarukala"
 VERSION = "1.0.0"
 
 def say_hi(msg:str = "Hi!", file_directory:str = "/app/data/") -> None:
@@ -11,7 +11,7 @@ def say_hi(msg:str = "Hi!", file_directory:str = "/app/data/") -> None:
 
     # Define filename with timestamp
     file_name = f"outputfile_{USER_NAME}_{VERSION}_timestamp_{timestamp}.txt"
-    file_path = os.join(file_directory, file_name)
+    file_path = os.path.join(file_directory, file_name)
 
     # Write the timestamp inside the file
     with open(file_path, "w") as file:
